2c340eb81c437b9df11946834e077d9b5b130db6
Wongsathon Chuijit <wongsathon@Wongsathons-MacBook-Air.local>
2024-10-03 20:05:31 +0700

    feat:picture fix

--- app/reviews/[slug]/page.tsx
+++ app/reviews/[slug]/page.tsx
@@ -85,7 +85,7 @@ export default async function ReviewPage({
           </div>
           <div className="flex flex-col gap-2 ">
             <div className="flex gap-2 items-center">
-              <img src="/images/ig.jpg" className="w-6 h-6" />
+              <img src="/images/iG.jpg" className="w-6 h-6" />
               <span>Indie Gamer score : {indie_score}</span>
             </div>
             <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
